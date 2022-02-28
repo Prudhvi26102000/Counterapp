47a0d4c42a687f00975868b110185e045ad20faf
Prudhvi26102000 <dasariprudhvisai26@gmail.com>
2022-02-28 16:23:46 +0530

    first commit

---
+++ counter-app/src/App.js
@@ -0,0 +1,55 @@
+import NavBar from './components/navbar';
+import './App.css';
+import Counters from './components/counters';
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { 
+    counters:[
+        { id: 1,value: 4},
+        { id: 2,value: 0},
+        { id: 3,value: 0},
+        { id: 4,value: 0}
+    ]
+ }; 
+
+ handleIncrement = counter =>{
+    const counters=[...this.state.counters];
+    const index=counters.indexOf(counter);
+    counters[index]={...counter };
+    counters[index].value++;
+    this.setState({counters});
+ };
+ handleReset=()=>{
+   const counters= this.state.counters.map(c=>{
+        c.value=0;
+        return c;
+    });
+    this.setState({counters});
+ };
+
+ handleDelete = (counterId) =>
+ {
+    //console.log("Event called",counterId);
+    const counters=this.state.counters.filter(c =>c.id !== counterId);
+    this.setState({ counters});
+    //console.log(counters);
+ }
+  render(){
+  return (
+    <React.Fragment>
+    <NavBar totalCounter={this.state.counters.filter(c=>c.value>0).length}/>
+    <main className="container">
+      <Counters 
+      onReset={this.handleReset}
+      onDelete={this.handleDelete}
+      onIncrement={this.handleIncrement}
+      counters={this.state.counters}
+      />
+    </main>
+    </React.Fragment>
+  );
+}
+}
+
+export default App;
